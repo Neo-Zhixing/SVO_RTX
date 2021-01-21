cda1b96716f4c7382342ad2e727839e84a89c118
Zhixing Zhang <me@neoto.xin>
2021-01-21 13:49:54 -0500

    Multichunk rendering

--- examples/mcanvil.rs
+++ examples/mcanvil.rs
@@ -44,7 +44,7 @@ fn setup(
     mut material_palettes: ResMut<Assets<MaterialPalette>>,
 ) {
     let mut colored_material = ColoredMaterial::default();
-    let scale: f32 = 8.0;
+    let scale: f32 = 1.0;
     let stone_material = Material {
         name: "stone".into(),
         scale,
@@ -87,6 +87,10 @@ fn setup(
         .unwrap();
 
 
+    let args: Vec<_> = std::env::args().skip(1).collect();
+    assert_eq!(args.len(), 1, "Format: mcanvil <mca filepath>");
+    let region_dir = args[0].clone();
+
     let colored_voxel = palette.add_colored_material(colored_material);
     let grass_voxel = palette.add_colored_material(grass_material);
     let leaves_voxel = palette.add_colored_material(leaves_material);
@@ -94,80 +98,91 @@ fn setup(
     let log_voxel = palette.add_material(log_material);
     let dirt_voxel = palette.add_material(dirt_material);
 
-    let mut octree: Octree<Voxel> = Octree::new();
+
 
     let args: Vec<_> = std::env::args().skip(1).collect();
     assert_eq!(args.len(), 1, "Format: mcanvil <mca filepath>");
-    let file = std::fs::File::open(args[0].clone()).unwrap();
 
-    let mut region = fastanvil::Region::new(file);
-    let chunk_data = region.load_chunk(0, 1).unwrap();
-    let chunk: fastanvil::Chunk = fastnbt::de::from_bytes(chunk_data.as_slice()).unwrap();
 
-    if let Some(sections) = chunk.level.sections {
-        for section in sections {
-            if section.palette.is_none() {
-                continue;
-            }
-            let palette = section.palette.unwrap();
-            if let Some(block_states) = section.block_states {
-                let bits_per_item = (block_states.0.len() * 8) / 4096;
-                let mut buff: [u16; 4096] = [0; 4096];
-                block_states.unpack_into(bits_per_item, &mut buff);
-                for (i, indice) in buff.iter().enumerate() {
-                    let indice = *indice;
-                    let block = &palette[indice as usize];
-                    let x = (i & 0xF) as u32;
-                    let z = ((i >> 4) & 0xF) as u32;
-                    let y = (i >> 8) as u32;
+    let mut load_region = |region_x: usize, region_y: usize| {
+        let file = std::fs::File::open(format!("{}/r.{}.{}.mca", region_dir, region_x, region_y)).unwrap();
+        let mut region = fastanvil::Region::new(file);
 
-                    let y= y + section.y as u32 * 16;
-                    assert_eq!(i >> 12, 0);
-                    let voxel = match block.name {
-                        "minecraft:air" => continue,
-                        "minecraft:cave_air" => continue,
-                        "minecraft:stone" => stone_voxel,
-                        "minecraft:granite" => stone_voxel,
-                        "minecraft:gravel" => stone_voxel,
-                        "minecraft:diorite" => stone_voxel,
-                        "minecraft:iron_ore" => stone_voxel,
-                        "minecraft:coal_ore" => stone_voxel,
-                        "minecraft:andesite" => stone_voxel,
-                        "minecraft:bedrock" => stone_voxel,
-                        "minecraft:grass" => continue,
-                        "minecraft:tall_grass" => continue,
-                        "minecraft:grass_block" => grass_voxel,
-                        "minecraft:oak_log" => log_voxel,
-                        "minecraft:oak_leaves" => leaves_voxel,
-                        "minecraft:acacia_leaves" => leaves_voxel,
-                        "minecraft:dirt" => dirt_voxel,
-                        _ => {
-                            println!("Missing block: w {:?}", block.name);
-                            colored_voxel
+        let mut octree: Octree<Voxel> = Octree::new();
+        region.for_each_chunk(|chunk_x, chunk_z, chunk_data| {
+            println!("loading chunk {} {}", chunk_x, chunk_z);
+            let chunk: fastanvil::Chunk = fastnbt::de::from_bytes(chunk_data.as_slice()).unwrap();
+
+            if let Some(sections) = chunk.level.sections {
+                for section in sections {
+                    if section.palette.is_none() {
+                        continue;
+                    }
+                    let palette = section.palette.unwrap();
+                    if let Some(block_states) = section.block_states {
+                        let bits_per_item = (block_states.0.len() * 8) / 4096;
+                        let mut buff: [u16; 4096] = [0; 4096];
+                        block_states.unpack_into(bits_per_item, &mut buff);
+                        for (i, indice) in buff.iter().enumerate() {
+                            let indice = *indice;
+                            let block = &palette[indice as usize];
+                            let x = (i & 0xF) as u32;
+                            let z = ((i >> 4) & 0xF) as u32;
+                            let y = (i >> 8) as u32;
+
+                            let y= y + section.y as u32 * 16;
+                            assert_eq!(i >> 12, 0);
+                            let voxel = match block.name {
+                                "minecraft:air" => continue,
+                                "minecraft:cave_air" => continue,
+                                "minecraft:stone" => stone_voxel,
+                                "minecraft:granite" => stone_voxel,
+                                "minecraft:gravel" => stone_voxel,
+                                "minecraft:diorite" => stone_voxel,
+                                "minecraft:iron_ore" => stone_voxel,
+                                "minecraft:coal_ore" => stone_voxel,
+                                "minecraft:andesite" => stone_voxel,
+                                "minecraft:bedrock" => stone_voxel,
+                                "minecraft:grass" => continue,
+                                "minecraft:tall_grass" => continue,
+                                "minecraft:grass_block" => grass_voxel,
+                                "minecraft:oak_log" => log_voxel,
+                                "minecraft:oak_leaves" => leaves_voxel,
+                                "minecraft:acacia_leaves" => leaves_voxel,
+                                "minecraft:dirt" => dirt_voxel,
+                                _ => {
+                                    //println!("Missing block: w {:?}", block.name);
+                                    colored_voxel
+                                }
+                            };
+                            octree.set(x + chunk_x as u32 * 16, y, z + chunk_z as u32 * 16, 512, voxel);
                         }
-                    };
-                    octree.set(x, y, z, 256, voxel);
+                    }
                 }
             }
-        }
-    }
+        });
 
+        let chunk = Chunk::new(octree, Vec4::new((region_x * 512) as f32, 0.0, (region_y * 512) as f32, 512.0));
+        let chunk_handle = chunks.add(chunk);
+        commands
+            .spawn(ChunkBundle::new(chunk_handle));
+    };
 
-    let chunk = Chunk::new(octree, Vec4::new(0.0, 0.0, 0.0, 16.0));
-
-    let chunk_handle = chunks.add(chunk);
+    load_region(1, 0);
+    load_region(0, 0);
     commands
-        .spawn(ChunkBundle::new(chunk_handle))
         .spawn(Camera3dBundle {
             transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0))
                 .looking_at(Vec3::default(), Vec3::unit_y()),
             perspective_projection: PerspectiveProjection {
                 near: 0.1,
+                far: 16384.0,
                 ..Default::default()
             },
             ..Default::default()
         })
         .with(FlyCamera::default());
+
 }
 
 fn my_system(mut sun_light_resource: ResMut<SunLight>, time: Res<Time>) {
@@ -175,4 +190,3 @@ fn my_system(mut sun_light_resource: ResMut<SunLight>, time: Res<Time>) {
     sun_light_resource.direction.z = (time.seconds_since_startup()).sin() as f32;
     sun_light_resource.direction.y = -1.0;
 }
-
--- src/material/material_node.rs
+++ src/material/material_node.rs
@@ -41,92 +41,115 @@ impl SystemNode for MaterialNode {
             system.id(),
             MaterialNodeState {
                 command_queue: self.command_queue.clone(),
+                event_reader: Default::default()
             },
         );
         Box::new(system)
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct MaterialNodeState {
     command_queue: CommandQueue,
+    pub event_reader: EventReader<AssetEvent<MaterialPalette>>,
 }
 
 pub fn material_node_system(
     mut state: Local<MaterialNodeState>,
     render_resource_context: Res<Box<dyn RenderResourceContext>>,
     mut palettes: ResMut<Assets<MaterialPalette>>,
+    texture_events: Res<Events<AssetEvent<MaterialPalette>>>,
     mut query: Query<(&Handle<MaterialPalette>, &mut RenderPipelines)>,
 ) {
+
     let render_resource_context = &**render_resource_context;
+    for event in state.event_reader.iter(&texture_events) {
+        match event {
+            AssetEvent::Created { handle } => {
+                println!("created material palette");
+                let mut palette = palettes.get_mut(handle).unwrap();
+                if let Some(staging_buffer) = palette.staging_buffer {
+                    println!("removed staging buffer");
+                    render_resource_context.remove_buffer(staging_buffer);
+                    palette.staging_buffer = None;
+                }
+
+                let colored_materials_size = palette.colored_materials_size();
+                let colored_materials_size_aligned =
+                    align_to(colored_materials_size, wgpu::BIND_BUFFER_ALIGNMENT as usize);
+                let total_size = colored_materials_size_aligned + palette.materials_size();
+                let total_size_aligned = align_to(total_size, wgpu::COPY_BUFFER_ALIGNMENT as usize);
+
+                if palette.buffer.is_none() {
+                    let staging_buffer = render_resource_context.create_buffer(BufferInfo {
+                        size: total_size_aligned,
+                        buffer_usage: BufferUsage::COPY_SRC | BufferUsage::MAP_WRITE,
+                        mapped_at_creation: true,
+                    });
+                    render_resource_context.write_mapped_buffer(
+                        staging_buffer,
+                        0..(total_size as u64),
+                        &mut |data: &mut [u8], _renderer| {
+                            // Color palette
+                            palette.colored_materials_write_bytes(&mut data[0..colored_materials_size]);
+                            palette.materials_write_bytes(
+                                &mut data[colored_materials_size_aligned..total_size],
+                            );
+                        },
+                    );
+                    render_resource_context.unmap_buffer(staging_buffer);
+
+                    let buffer = render_resource_context.create_buffer(BufferInfo {
+                        size: total_size_aligned,
+                        buffer_usage: BufferUsage::COPY_DST | BufferUsage::STORAGE,
+                        mapped_at_creation: false,
+                    });
+                    state.command_queue.copy_buffer_to_buffer(
+                        staging_buffer,
+                        0,
+                        buffer,
+                        0,
+                        total_size_aligned as u64,
+                    );
+                    palette.staging_buffer = Some(staging_buffer);
+                    palette.buffer = Some(buffer);
+                }
+            }
+            AssetEvent::Modified { handle } => {
+                println!("palette modified");
+            }
+            AssetEvent::Removed { handle } => {
+                println!("palette removed")
+            }
+        }
+    }
+
+    fn align_to(num: usize, alignment: usize) -> usize {
+        ((num + alignment - 1) / alignment) * alignment
+    }
 
     for (palette_handle, mut render_pipelines) in query.iter_mut() {
-        let mut palette = palettes.get_mut(palette_handle).unwrap();
-        if let Some(staging_buffer) = palette.staging_buffer {
-            render_resource_context.remove_buffer(staging_buffer);
-            palette.staging_buffer = None;
-        }
-
-        fn align_to(num: usize, alignment: usize) -> usize {
-            ((num + alignment - 1) / alignment) * alignment
-        }
-
+        let palette = palettes.get(palette_handle).unwrap();
         let colored_materials_size = palette.colored_materials_size();
         let colored_materials_size_aligned =
             align_to(colored_materials_size, wgpu::BIND_BUFFER_ALIGNMENT as usize);
         let total_size = colored_materials_size_aligned + palette.materials_size();
         let total_size_aligned = align_to(total_size, wgpu::COPY_BUFFER_ALIGNMENT as usize);
-
-        if palette.buffer.is_none() {
-            let staging_buffer = render_resource_context.create_buffer(BufferInfo {
-                size: total_size_aligned,
-                buffer_usage: BufferUsage::COPY_SRC | BufferUsage::MAP_WRITE,
-                mapped_at_creation: true,
-            });
-            render_resource_context.write_mapped_buffer(
-                staging_buffer,
-                0..(total_size as u64),
-                &mut |data: &mut [u8], _renderer| {
-                    // Color palette
-                    palette.colored_materials_write_bytes(&mut data[0..colored_materials_size]);
-                    palette.materials_write_bytes(
-                        &mut data[colored_materials_size_aligned..total_size],
-                    );
-                },
-            );
-            render_resource_context.unmap_buffer(staging_buffer);
-
-            let buffer = render_resource_context.create_buffer(BufferInfo {
-                size: total_size_aligned,
-                buffer_usage: BufferUsage::COPY_DST | BufferUsage::STORAGE,
-                mapped_at_creation: false,
-            });
-            state.command_queue.copy_buffer_to_buffer(
-                staging_buffer,
-                0,
-                buffer,
-                0,
-                total_size_aligned as u64,
-            );
-            palette.staging_buffer = Some(staging_buffer);
-            palette.buffer = Some(buffer);
-
-            render_pipelines.bindings.set(
-                "ColoredMaterials",
-                RenderResourceBinding::Buffer {
-                    buffer: palette.buffer.unwrap(),
-                    range: 0..colored_materials_size_aligned as u64,
-                    dynamic_index: None,
-                },
-            );
-            render_pipelines.bindings.set(
-                "Materials",
-                RenderResourceBinding::Buffer {
-                    buffer: palette.buffer.unwrap(),
-                    range: colored_materials_size_aligned as u64..total_size_aligned as u64,
-                    dynamic_index: None,
-                },
-            );
-        }
+        render_pipelines.bindings.set(
+            "ColoredMaterials",
+            RenderResourceBinding::Buffer {
+                buffer: palette.buffer.unwrap(),
+                range: 0..colored_materials_size_aligned as u64,
+                dynamic_index: None,
+            },
+        );
+        render_pipelines.bindings.set(
+            "Materials",
+            RenderResourceBinding::Buffer {
+                buffer: palette.buffer.unwrap(),
+                range: colored_materials_size_aligned as u64..total_size_aligned as u64,
+                dynamic_index: None,
+            },
+        );
     }
 }
